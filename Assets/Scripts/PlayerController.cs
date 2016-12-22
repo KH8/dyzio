cda54b5d7a68332b059bb80752853c453ea05baa
kh8 <reczek.krzysztof@gmail.com>
2016-12-22 19:24:44 +0100

    Cursor visibility switched off

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,14 @@ public class PlayerController : MonoBehaviour {
     private float _speedModificator = 1.0f;
 
     /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start() {
+        Cursor.visible = false;
+    }
+
+    /// <summary>
     /// Awake is called when the script instance is being loaded.
     /// </summary>
     void Awake() {
