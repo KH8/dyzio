34d6134d870382e48388d47e500eadddc930bdf4
kh8 <reczek.krzysztof@gmail.com>
2017-01-08 23:08:52 +0100

    Touch chain implemented

--- Assets/Scripts/Movables/TouchableController.cs
+++ Assets/Scripts/Movables/TouchableController.cs
@@ -5,7 +5,6 @@ public class TouchableController : MonoBehaviour {
 
     private TouchCounter _tc;
 
-    private bool _touchable = true;
     private bool _touched;
 
     /// <summary>
@@ -15,21 +14,23 @@ public class TouchableController : MonoBehaviour {
         _tc = GameObject.Find("Game").GetComponent<TouchCounter>();
     }
 
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    void Update() {
-        if (_touched && transform.position.magnitude > touchThreshold) {
-            _touched = false;
-            _touchable = false;
+    public void Touch() {
+        if (!_touched && transform.position.magnitude > touchThreshold) {
+            _touched = true;
             _tc.Increment();
+            Debug.Log(this.name + " - touched");
         }
     }
 
-    public void Touch() {
-        if (_touchable) {
-            _touched = true;
-            Debug.Log(this.name + " - touched");
+    /// <summary>
+    /// OnCollisionStay is called once per frame for every collider/rigidbody
+    /// that is touching rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionStay(Collision other) {
+        TouchableController tc = other.gameObject.GetComponent<TouchableController>();
+        if (tc != null && _touched) {
+            tc.Touch();
         }
     }
 }
