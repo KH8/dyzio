ec99bd28ac9d673e6e4c149c7920898673875ba7
kh8 <reczek.krzysztof@gmail.com>
2017-01-18 19:12:59 +0100

    Mouse controlled bullet angle implemented

--- Assets/Scripts/Player/ShootingController.cs
+++ Assets/Scripts/Player/ShootingController.cs
@@ -2,24 +2,56 @@ using UnityEngine;
 
 public class ShootingController : MonoBehaviour {
     public GameObject bulletPrefab;
-    public Transform bulletSpawn;
+    public Transform bulletSpawnLeft;
+    public Transform bulletSpawnRight;
+
+    public float initialSpeed = 24.0f;
+    public float lifeTime = 2.0f;
+
+	private float _overheadAngle = 15.0f;
+	private float _overheadAngleMax = 45.0f;
+	private float _overheadAngleMin = -15.0f;
+
+    private bool _spawnSideToggle = false;
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update() {
-        if (Input.GetKeyDown(KeyCode.E)) {
+        CalculateOverheadAngle();
+        CalculateBulletSpawnPosition();
+        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.F)) {
             Fire();
         }
     }
 
     void Fire() {
+        var bulletSpawn = GetNextBulletSpawn();
         var bullet = (GameObject) Instantiate(
             bulletPrefab,
             bulletSpawn.position,
             bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * initialSpeed;
+        Destroy(bullet, lifeTime);
+    }
 
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 24.0f;
-        Destroy(bullet, 3.0f);
+    private Transform GetNextBulletSpawn() {
+        _spawnSideToggle = !_spawnSideToggle;
+        return _spawnSideToggle ? bulletSpawnLeft : bulletSpawnRight;
+    }
+
+	private void CalculateOverheadAngle() {
+		_overheadAngle += Input.GetAxis("Mouse Y"); 
+		_overheadAngle = Mathf.Max(_overheadAngle, _overheadAngleMin);
+		_overheadAngle = Mathf.Min(_overheadAngle, _overheadAngleMax);
+	}
+
+    private void CalculateBulletSpawnPosition() {
+		bulletSpawnLeft.rotation = GetSpawnAngles();
+		bulletSpawnRight.rotation = GetSpawnAngles();
+    }
+
+    private Quaternion GetSpawnAngles() {
+        return Quaternion.Euler(-1 * _overheadAngle + 105.0f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 }
